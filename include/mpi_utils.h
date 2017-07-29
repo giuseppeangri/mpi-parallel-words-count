a3e183444023cb39f9c52f210ac91612f7987d00
Giuseppe Angri <giuseppeangri@icloud.com>
2017-07-29 19:59:46 +0200

    Add MPI Utils

---
+++ include/mpi_utils.h
@@ -0,0 +1,2 @@
+extern void MPI_Print(int rank, const char * format, ...);
+extern void MPI_Print_To_File(FILE * log_file, int rank, const char * format, ...);
---
+++ src/mpi_utils.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+void MPI_Print(int rank, const char * format, ...) {
+	
+	for(int i=0; i<rank; i++)
+		printf("\t");
+
+	printf("RANK %02d - ", rank);
+	va_list args;
+	va_start(args, format);
+	vprintf(format, args);
+	va_end(args);
+	printf("\n");
+
+}
+
+void MPI_Print_To_File(FILE * log_file, int rank, const char * format, ...) {
+	
+	fprintf(log_file, "RANK %02d - ", rank);
+	va_list args;
+	va_start(args, format);
+	vfprintf(log_file, format, args);
+	va_end(args);
+	fprintf(log_file, "\n");
+
+}
