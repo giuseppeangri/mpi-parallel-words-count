28e3d9d8d2a654b58933fb339a183bffa14df313
Giuseppe Angri <giuseppeangri@icloud.com>
2017-07-29 19:59:32 +0200

    Add FileInformationContainer

---
+++ include/file_information_container.h
@@ -0,0 +1,11 @@
+typedef struct FileInformationContainer {
+
+	FileInformation ** files;
+	int num_files;
+	double total_size;
+
+} FileInformationContainer;
+
+extern FileInformationContainer FileInformationContainer_constructor();
+extern void FileInformationContainer_add(FileInformationContainer * fileInformationContainer, FileInformation * fileInformation);
+extern void FileInformationContainer_addByPath(FileInformationContainer * fileInformationContainer, char * filePath);
---
+++ src/file_information_container.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <file_information.h>
+#include <file_information_container.h>
+
+FileInformationContainer FileInformationContainer_constructor() {
+
+	FileInformationContainer fileInformationContainer;
+
+	fileInformationContainer.files = (FileInformation **) malloc(sizeof(FileInformation *));
+	fileInformationContainer.num_files = 0;
+	fileInformationContainer.total_size = 0;
+
+	return fileInformationContainer;
+
+}
+
+void FileInformationContainer_add(FileInformationContainer * fileInformationContainer, FileInformation * fileInformation) {
+
+	fileInformationContainer->num_files++;
+	fileInformationContainer->files = (FileInformation **) realloc(fileInformationContainer->files, sizeof(FileInformation *) * fileInformationContainer->num_files);
+
+	fileInformationContainer->files[fileInformationContainer->num_files-1] = fileInformation;
+
+	fileInformationContainer->total_size += fileInformation->size;
+
+}
+
+void FileInformationContainer_addByPath(FileInformationContainer * fileInformationContainer, char * filePath) {
+
+	FileInformation * fileInformation = FileInformation_constructor(filePath);
+
+	FileInformationContainer_add(fileInformationContainer, fileInformation);
+
+}
