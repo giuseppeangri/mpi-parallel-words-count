45fd646fa725614e1361545908e3feb0b862a0ce
Giuseppe Angri <giuseppeangri@icloud.com>
2017-10-19 15:28:49 +0200

    Update Main

--- src/main.c
+++ src/main.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <mpi.h>
 
 #include <mpi_utils.h>
-#include <file_reader.h>
+#include <file_utils.h>
+
 #include <file_information.h>
 #include <file_information_container.h>
 
-#include <ctype.h>
+#include <counter.h>
+#include <counter_container.h>
+
+#include <file_reader.h>
 
 FileInformationContainer makeContainer(char * masterFilePath);
 
@@ -37,19 +40,11 @@ int main(int argc, char * argv[]) {
 	FILE * log_file;
 	char * log_file_name;
 
-	FileInformationContainer container;
+	FileInformationContainer filesContainer;
 
-	double 	split_size;
+	CounterContainer entriesContainer;
 
-	FILE * 	file_to_read;
-	int 	file_to_read_index; 
-	double 	already_read_by_others;
-	double 	already_read_by_me;
-
-	char 	current_char;
-
-	char 	current_word[100];
-	int 	current_word_index;
+	double split_size;
 
 // MAIN
 
@@ -62,180 +57,41 @@ int main(int argc, char * argv[]) {
 	MPI_Print_To_File(log_file, my_rank, "MPI Words Count Collective - Giuseppe Angri");
 
 	// Get information about all files to read
-	container = makeContainer(argv[1] ? argv[1] : "../data/00_master.txt");
+	filesContainer = makeContainer(argv[1] ? argv[1] : "../data/00_master.txt");
 
 	// Calculate split size that each process will have to read
-	// split_size = round( (container.total_size / num_processes)*10 ) / 10;
-	split_size = (int) (container.total_size / num_processes);
+	// split_size = round( (filesContainer.total_size / num_processes)*10 ) / 10;
+	split_size = (int) (filesContainer.total_size / num_processes);
 
 	if(my_rank == master_rank) {
-		MPI_Print_To_File(log_file, my_rank, "Number of files: %d", container.num_files);
-		MPI_Print_To_File(log_file, my_rank, "Total size of files: %.2f bytes", container.total_size);
+		MPI_Print_To_File(log_file, my_rank, "Number of files: %d", filesContainer.num_files);
+		MPI_Print_To_File(log_file, my_rank, "Total size of files: %.2f bytes", filesContainer.total_size);
 		MPI_Print_To_File(log_file, my_rank, "Single split size: %f bytes\n\n", split_size);
 
-		MPI_Print(my_rank, "Number of files: %d", container.num_files);
-		MPI_Print(my_rank, "Total size of files: %.2f bytes", container.total_size);
+		MPI_Print(my_rank, "Number of files: %d", filesContainer.num_files);
+		MPI_Print(my_rank, "Total size of files: %.2f bytes", filesContainer.total_size);
 		MPI_Print(my_rank, "Single split size: %f bytes\n\n", split_size);
 	}
 
-	// Calculate the size already read by previous processes
-    already_read_by_others = my_rank * split_size;
+	entriesContainer = CounterContainer_constructor();
 
-    // Calculate the file to read and the remaining part of size already read by previous processes
-    file_to_read_index = 0;
-	
-    while(already_read_by_others >= container.files[file_to_read_index]->size) {
-    	already_read_by_others -= (container.files[file_to_read_index]->size);
-    	file_to_read_index++;
-    }
+	// CounterContainer_print(&entriesContainer);
 
-    already_read_by_others += my_rank-1;
+	// CounterContainer_addWord(&entriesContainer, "Lorem");
 
-    // Open the first file that my process must read
-    file_to_read = openFile(container.files[file_to_read_index]->path, "r");
+	// CounterContainer_print(&entriesContainer);
 
-    // Move in the part of file that my process must read
-    fseek(file_to_read, already_read_by_others, SEEK_SET);
+	// CounterContainer_addWord(&entriesContainer, "ipsum");
 
-    int offset = 0;
+	// CounterContainer_print(&entriesContainer);
 
-	if(already_read_by_others > 0) {
+	// CounterContainer_addWord(&entriesContainer, "ipsum");
 
-		// non parto dall'inizio
+	// CounterContainer_print(&entriesContainer);
 
-		current_char = fgetc(file_to_read);
+	// exit(0);
 
-		MPI_Print(my_rank, "file %d", file_to_read_index);
-		MPI_Print(my_rank, "already_read_by_others %f", already_read_by_others);
-		MPI_Print(my_rank, "start %c", current_char);
-
-		if(isalnum(current_char)) {
-
-			// se è un char torna indietro fino a spazio
-
-			while(isalnum(current_char)) {
-
-				offset++;
-				already_read_by_others--;
-
-				if(already_read_by_others >= 0) {
-					fseek(file_to_read, already_read_by_others, SEEK_SET);
-					current_char = fgetc(file_to_read);
-				}
-				else {
-					// sono tornato all'inizio del file
-					rewind(file_to_read);
-					break;
-				}
-				
-			}
-
-
-		}
-
-	}    
-
-    // Start reading
-    already_read_by_me = 0;
-    current_word_index = 0;
-
-    int word_founded = 0;
-
-    // Read my split size
-    while(already_read_by_me < (split_size+offset) ) {    	
-
-		current_char = fgetc(file_to_read);
-
-		if(current_char == EOF) {
-
-			if(current_word_index > 0) {
-				// In this case i read a space
-				// So i read a whole word
-
-				MPI_Print_To_File(log_file, my_rank, "Word readed: %s", current_word);
-				word_founded++;
-
-				// Clear current word and reset current word index
-				memset(&current_word[0], 0, sizeof(current_word));
-				current_word_index = 0;
-			}
-
-			// In this case the file is finished
-			// So i need to read the next available file
-
-			fclose(file_to_read);
-			file_to_read_index++;
-
-			// Check if all files was readed
-			if(file_to_read_index >= container.num_files)
-				break;
-
-			MPI_Print_To_File(log_file, my_rank, "Skip to file: %d", file_to_read_index);
-
-			file_to_read = openFile(container.files[file_to_read_index]->path, "r");
-
-			continue;
-		}
-
-		if( isalnum(current_char) ) {
-
-			// In this case the current char is an alphanumeric character
-			// So i need to add this to current word
-
-			current_word[current_word_index] = current_char;
-			current_word_index++;
-
-		}
-		else if( isspace(current_char) || iscntrl(current_char) ) {
-
-			if(current_word_index > 0) {
-				// In this case i read a space
-				// So i read a whole word
-
-				MPI_Print_To_File(log_file, my_rank, "Word readed: %s", current_word);
-				word_founded++;
-
-				// Clear current word and reset current word index
-				memset(&current_word[0], 0, sizeof(current_word));
-				current_word_index = 0;
-			}
-
-		}
-
-		// Anyway i must increase the part readed by me
-		already_read_by_me++;
-
-    }
-
-    MPI_Print(my_rank, "end %c", current_char);
-
-    MPI_Print_To_File(log_file, my_rank, "I read this size: %f", already_read_by_me);
-    MPI_Print(my_rank, "I read this size: %f", already_read_by_me);
-
-    current_char = fgetc(file_to_read);
-
-    MPI_Print(my_rank, "next end %c", current_char);
-
-    if( (!isalnum(current_char)) ) {
-
-    	if(current_word_index > 0) {
-
-	    	// In this case i read a space
-			// So i read a whole word
-
-			MPI_Print_To_File(log_file, my_rank, "Final Word readed: %s", current_word);
-			word_founded++;
-
-			// Clear current word and reset current word index
-			memset(&current_word[0], 0, sizeof(current_word));
-			current_word_index = 0;
-
-		}
-
-    }
-
-    MPI_Print_To_File(log_file, my_rank, "Word Founded: %d", word_founded);
-    MPI_Print(my_rank, "Word Founded: %d", word_founded);
+	startRead(my_rank, split_size, filesContainer, entriesContainer, log_file);
 
     int rec = 1;
     int rv[10];
@@ -244,8 +100,6 @@ int main(int argc, char * argv[]) {
     if(my_rank == master_rank)
     	MPI_Print_To_File(log_file, my_rank, "END");
 
-
-    fclose(file_to_read);
     fclose(log_file);
 
 // MPI CLOSE
@@ -259,19 +113,19 @@ FileInformationContainer makeContainer(char * masterFilePath) {
 
 	FILE * masterFile = fopen(masterFilePath, "r");
 
-	FileInformationContainer container = FileInformationContainer_constructor();
+	FileInformationContainer filesContainer = FileInformationContainer_constructor();
 
     char **pathList = readAllLines(masterFile);
 
     char **singlePath = pathList;
 
     while(*singlePath != NULL) {
-    	FileInformationContainer_addByPath(&container, *singlePath);
+    	FileInformationContainer_addByPath(&filesContainer, *singlePath);
     	++singlePath;
     }
 
     fclose(masterFile);
 
-	return container;
+	return filesContainer;
 
 }
