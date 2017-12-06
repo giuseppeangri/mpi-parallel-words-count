3f3ebec5dd33676c958aa5cc57bdd591f5305184
Giuseppe Angri <giuseppeangri@icloud.com>
2017-12-06 18:15:32 +0100

    Add File Information Container MPI

---
+++ include/file_information_container_mpi.h
@@ -0,0 +1,2 @@
+extern int    FileInformationContainer_calculateSendPackSize(FileInformationContainer * filesContainer);
+extern void * FileInformationContainer_makeSendPackBuffer(FileInformationContainer * filesContainer, int sendPackSize);
