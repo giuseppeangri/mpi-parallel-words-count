0bf105446a3e80e038a1d874ebcadd7eaec8e7d4
Giuseppe Angri <giuseppeangri@icloud.com>
2018-01-23 23:38:46 +0100

    Buf fix on memset

--- src/file_reader.c
+++ src/file_reader.c
@@ -77,7 +77,7 @@ void checkStartingPoint(int my_rank, double split_size, FileInformationContainer
 
 void resetWordBuffer(char * word, int * word_index) {
 	// Clear current word and reset current word index
-	memset(&word[0], 0, strlen(word));
+	memset(&word[0], 0, 100*sizeof(char));
 	*word_index = 0;
 }
 
